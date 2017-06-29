3b7145c8193fb063f3c5560979115693b2e7d050
danile zhang <daniel3.zhang@sonymobile.com>
2017-06-29 05:52:54 +0200

    Run  status check

--- test.sh
+++ test.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 [ $# -lt 1 ]&& echo "Need Input a status" && exit 1
 STATUS="$1"
-while true;
-do
 if [ "$STATUS" == "UPDATE_COMPLETE" ] ; then
   echo "Adding change to git repository"
-  break
 elif [ "$STATUS" == "UPDATE_ROLLBACK_COMPLETE" ] ;then
 	echo "Error: CloudFormation status ($STATUS), cannot continue automatically.."
 	exit 1
 else
   echo "Error: Unknown CloudFormation status ($STATUS), cannot continue automatically.."
 fi
-done
